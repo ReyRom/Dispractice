b91111797f8f9ee12b62180c99c040818932f6d4
Roman Sadovskiy <s03roman@mail.ru>
2025-01-06 01:30:37 +0300

    navigation service

--- Dispractice/App.axaml.cs
+++ Dispractice/App.axaml.cs
@@ -3,6 +3,7 @@ using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Data.Core.Plugins;
 using Avalonia.Markup.Xaml;
 using Dispractice.Extensions;
+using Dispractice.Services;
 using Dispractice.ViewModels;
 using Dispractice.Views;
 using Microsoft.Extensions.DependencyInjection;
@@ -11,6 +12,7 @@ namespace Dispractice;
 
 public partial class App : Application
 {
+    public static ServiceProvider Services { get; private set; }
     public override void Initialize()
     {
         AvaloniaXamlLoader.Load(this);
@@ -20,15 +22,16 @@ public partial class App : Application
     {
         // Line below is needed to remove Avalonia data validation.
         // Without this line you will get duplicate validations from both Avalonia and CT
-        // Line below is needed to remove Avalonia data validation.
-        // Without this line you will get duplicate validations from both Avalonia and CT
         BindingPlugins.DataValidators.RemoveAt(0);
 
         // Register all the services needed for the application to run
         var collection = new ServiceCollection();
         collection.AddCommonServices();
+        collection.AddSingleton<NavigationService>();
+        collection.AddSingleton<ServicemanService>();
+
         // Creates a ServiceProvider containing services from the provided IServiceCollection
-        var services = collection.BuildServiceProvider();
+        Services = collection.BuildServiceProvider();
 
         if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
         {
---
+++ Dispractice/Services/NavigationService.cs
@@ -0,0 +1,37 @@
+﻿using Dispractice.ViewModels;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dispractice.Services
+{
+    public class NavigationService
+    {
+        Stack<ViewModelBase> _navigationStack = new Stack<ViewModelBase>();
+
+        public ViewModelBase Current => _navigationStack.Peek();
+        public int Count => _navigationStack.Count;
+
+        public void NavigateTo<T>(T viewModel) where T : ViewModelBase
+        {
+            _navigationStack.Push(viewModel);
+        }
+
+        public void NavigateTo<T>() where T : ViewModelBase
+        {
+            var viewModel = App.Services.GetRequiredService<T>();
+            _navigationStack.Push(viewModel);
+        }
+
+        public void GoBack(int steps = 1)
+        {
+            for (int i = 0; i < steps; i++)
+            {
+                _navigationStack.Pop();
+            }
+        }
+    }
+}
